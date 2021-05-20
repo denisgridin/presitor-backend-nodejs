76ca21124c9bbaa44b50da2a2d3e2f7e4ce11768
DenisGridin <denisgridin6@gmail.com>
2021-05-20 18:30:50 +0300

    disable node modules cache

--- src/controller/elements-controller.ts
+++ src/controller/elements-controller.ts
@@ -1,13 +1,13 @@
 import {Body, Delete, Get, JsonController, Param, Post, Put, Req, Res, UseBefore} from "routing-controllers";
-import {FIELDS, PATH} from "@/utils/constants";
+import {FIELDS, PATH} from "@/utils/constants.js";
 import {Error} from '@/utils/Error.js';
 import {MESSAGES} from "@/utils/messages.js";
-import {errorCodes} from "@/utils/errorCodes";
-import {log} from "@/utils/logger";
+import {errorCodes} from "@/utils/errorCodes.js";
+import {log} from "@/utils/logger.js";
 import {Response} from "express";
 import {checkInstancesExisting, checkUserPresentationAccess} from "../middleware/middleware";
-import { ElementModel } from "@/model/element";
-import {IElement} from "@/interface/presentation";
+import { ElementModel } from "@/model/element.js";
+import {IElement} from "@/interface/presentation.js";
 import uuid from "uuid-random";
 import axios from 'axios'
 import * as fs from "fs"
